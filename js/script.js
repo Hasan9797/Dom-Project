57f5201ed54d311c4e1c826f5f6629b0b06a8c7c
Hasanjon <hasanjonsadullayev97@gmail.com>
2022-02-26 18:19:19 +0500

    Easy Slider ++

--- js/script.js
+++ js/script.js
@@ -138,4 +138,48 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadline);
+
+
+    // Easy Slider
+
+    const sliderWrapper = document.querySelector('.offer__slider-wrapper'),
+        slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        current = document.querySelector('#current'),
+        total = document.querySelector('#total');
+
+    let slideIndex = 1;
+    show(slideIndex)
+
+    function show(s){
+        if(s > slides.length){
+            slideIndex = 1
+        }
+        if (s < 1){
+            slideIndex = slides.length
+        }
+
+        slides.forEach(item => item.classList.add('none'))
+        slides[slideIndex-1].classList.add('show')
+        slides[slideIndex-1].classList.remove('none')
+
+        if (slides.length < 10){
+            current.textContent = `0${slideIndex}`
+        }else {
+            current.textContent = slideIndex
+        }
+    }
+
+    function sliderPlus(s){
+        show(slideIndex += s)
+    }
+
+    prev.addEventListener('click', () => {
+        sliderPlus(-1)
+    })
+
+    next.addEventListener('click', () => {
+        sliderPlus(1)
+    })
 })
